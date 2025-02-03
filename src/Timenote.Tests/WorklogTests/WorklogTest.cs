dcf5f2fa88952cdefed06ae1fd229a77782291c9
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-02-03 21:56:36 +0100

    Add unit test for check adding multiple entries

--- src/Timenote.Tests/WorklogTests/WorklogTest.cs
+++ src/Timenote.Tests/WorklogTests/WorklogTest.cs
@@ -14,8 +14,8 @@ public class WorklogTest
         _worklogService = new WorklogService();
     }
     
-    [Test, Description("Entry can be added to Worklog")]
-    public void EntryCanBeAddedToWorklog()
+    [Test, Description("Single entry can be added to Worklog")]
+    public void SingleEntryCanBeAddedToWorklog()
     {
         // arrange
         var startTime = new DateTime(2025, 01, 01, 08, 00, 00);
@@ -32,5 +32,42 @@ public class WorklogTest
         var entriesCollection = _worklogService.GetEntries();
         
         Assert.That(entriesCollection, Has.Count.EqualTo(1));
+    }
+    
+    [Test, Description("Multiple entries can be added to Worklog")]
+    public void MultipleEntriesCanBeAddedToWorklog()
+    {
+        // arrange
+        var entriesToAdd = new List<Entry>()
+        {
+            new()
+            {
+                StartTime = new DateTime(2025, 01, 01, 08, 00, 00),
+                EndTime = new DateTime(2025, 01, 01, 16, 00, 00),
+            },
+            new()
+            {
+                StartTime = new DateTime(2025, 01, 01, 09, 00, 00),
+                EndTime = new DateTime(2025, 01, 01, 17, 00, 00),
+            },
+            new()
+            {
+                StartTime = new DateTime(2025, 01, 01, 06, 00, 00),
+                EndTime = new DateTime(2025, 01, 01, 15, 00, 00),
+            }
+        };
+        
+        var expectedCount = entriesToAdd.Count;
+      
+        // act
+        foreach (var entry in entriesToAdd)
+        {
+            _worklogService.AddEntry(entry);
+        }
+        
+        // assert
+        var entriesCollection = _worklogService.GetEntries();
+        
+        Assert.That(entriesCollection, Has.Count.EqualTo(expectedCount));
     }
 }
