90374e8cb8bd92e0c0f7e974b6040a969e8c2083
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-02-07 22:50:49 +0100

    Add database context for persistence layer

---
+++ src/Timenote.Persistence/Context/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Timenote.Domain.Entities;
+
+namespace Timenote.Persistence.Context;
+
+public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
+{
+    public DbSet<Worklog> Worklogs { get; set; } = null!;
+    public DbSet<Entry> Entries  { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Worklog>()
+            .HasKey(worklog => worklog.Id);
+        
+        modelBuilder.Entity<Entry>()
+            .HasOne(entry => entry.Worklog)
+            .WithMany(worklog => worklog.Entries)
+            .HasForeignKey(entry => entry.WorklogId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
