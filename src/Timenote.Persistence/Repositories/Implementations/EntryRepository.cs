21e81cdc65fa6751c591f5ce47ac1e20863e3311
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-02-07 22:51:05 +0100

    Create IEntryRepository

---
+++ src/Timenote.Persistence/Repositories/Implementations/EntryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Timenote.Domain.Entities;
+using Timenote.Persistence.Context;
+using Timenote.Persistence.Repositories.Abstractions;
+
+namespace Timenote.Persistence.Repositories.Implementations;
+
+public class EntryRepository(DatabaseContext context) : IEntryRepository
+{
+    public void Add(Entry entry)
+    {
+        context.Entries.Add(entry);
+        context.SaveChanges();
+    }
+}
