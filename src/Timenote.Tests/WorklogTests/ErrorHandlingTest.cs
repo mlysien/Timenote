8cc12b8fc313fc0316fb1a908aac4107baa9130a
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-02-10 19:53:36 +0100

    Add unit test for check if EndTime can be earlier than StartTime

---
+++ src/Timenote.Tests/WorklogTests/ErrorHandlingTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using Timenote.Core.Services.Implementations;
+using Timenote.Domain.Entities;
+using Timenote.Domain.Exceptions;
+using Timenote.Persistence.Context;
+using Timenote.Persistence.Repositories.Implementations;
+
+namespace Timenote.Tests.WorklogTests;
+
+public class ErrorHandlingTest
+{
+    private DbContextOptions<DatabaseContext> _dbContextOptions;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _dbContextOptions = new DbContextOptionsBuilder<DatabaseContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+    }
+    
+    [Test, Description("Worklog entry EndTime cannot be earlier than StartTime")]
+    public void EntryEndTimeCannotBeEarlierThanStartTime()
+    {
+        // arrange
+        using var context = new DatabaseContext(_dbContextOptions);
+        var repository = new EntryRepository(context);
+        var service = new WorklogService(repository);
+        var entry = new Entry
+        {
+            StartTime = new DateTime(2025, 01, 01, 09, 0, 0),
+            EndTime = new DateTime(2025, 01, 01, 08, 0, 0),
+            ProjectId = Guid.NewGuid()
+        };
+        
+        // act & assert
+        Assert.Multiple(() =>
+        {
+            var exception = Assert.Throws<InvalidWorklogEntryException>(() => service.AddWorklogEntry(entry));
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception!.Message, Is.Not.Empty);
+        });
+    }
+}
