8adbcb2a01dc4a36d259ab2ea93c32b1631eaa0b
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-02-12 21:57:39 +0100

    Refactor error handling test
    Remove DatabaseContext dependency

--- src/Timenote.Tests/WorklogTests/ErrorHandlingTests.cs
+++ src/Timenote.Tests/WorklogTests/ErrorHandlingTests.cs
@@ -1,32 +1,29 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Moq;
+using Timenote.Core.Services.Abstractions;
 using Timenote.Core.Services.Implementations;
 using Timenote.Domain.Entities;
 using Timenote.Domain.Exceptions;
-using Timenote.Persistence.Context;
-using Timenote.Persistence.Repositories.Implementations;
+using Timenote.Persistence.Repositories.Abstractions;
 
 namespace Timenote.Tests.WorklogTests;
 
 [TestFixture(TestName = "Error handling tests", Description = "Contains tests for all error cases")]
 public class ErrorHandlingTests
 {
-    private DbContextOptions<DatabaseContext> _dbContextOptions;
+    private Mock<IEntryRepository> _entryRepositoryMock;
+    private IWorklogService _worklogService;
     
     [SetUp]
     public void Setup()
     {
-        _dbContextOptions = new DbContextOptionsBuilder<DatabaseContext>()
-            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-            .Options;
+        _entryRepositoryMock = new Mock<IEntryRepository>();
+        _worklogService = new WorklogService(_entryRepositoryMock.Object);
     }
     
     [Test, Description("Worklog entry EndTime cannot be earlier than StartTime")]
     public void EntryEndTimeCannotBeEarlierThanStartTime()
     {
         // arrange
-        using var context = new DatabaseContext(_dbContextOptions);
-        var repository = new EntryRepository(context);
-        var service = new WorklogService(repository);
         var entry = new Entry
         {
             StartTime = new DateTime(2025, 01, 01, 09, 0, 0),
@@ -37,7 +34,7 @@ public class ErrorHandlingTests
         // act & assert
         Assert.Multiple(() =>
         {
-            var exception = Assert.Throws<InvalidWorklogEntryException>(() => service.AddWorklogEntry(entry));
+            var exception = Assert.Throws<InvalidWorklogEntryException>(() => _worklogService.AddWorklogEntry(entry));
             Assert.That(exception, Is.Not.Null);
             Assert.That(exception!.Message, Is.Not.Empty);
         });
