6d5c79736c3282d53b03ba87a21e4a51e73cffe8
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-02-13 17:27:08 +0100

    Create IProjectService with implementation

---
+++ src/Timenote.Core/Services/Abstractions/IProjectService.cs
@@ -0,0 +1,8 @@
+﻿using Timenote.Domain.Entities;
+
+namespace Timenote.Core.Services.Abstractions;
+
+public interface IProjectService
+{
+    Task CreateProjectAsync(Project project);
+}
---
+++ src/Timenote.Core/Services/Implementations/ProjectService.cs
@@ -0,0 +1,13 @@
+﻿using Timenote.Core.Services.Abstractions;
+using Timenote.Domain.Entities;
+using Timenote.Persistence.Repositories.Abstractions;
+
+namespace Timenote.Core.Services.Implementations;
+
+public class ProjectService(IProjectRepository projectRepository) : IProjectService
+{
+    public async Task CreateProjectAsync(Project project)
+    {
+        await projectRepository.Add(project);
+    }
+}
