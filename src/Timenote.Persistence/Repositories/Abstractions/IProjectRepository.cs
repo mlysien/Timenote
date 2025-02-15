77e7f9a32477cc141f7a5fd19f50e20072ba01c4
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-02-15 13:52:42 +0100

    Add new methods to IProjectRepository
    for get by Id and for check if project exists

--- src/Timenote.Persistence/Repositories/Abstractions/IProjectRepository.cs
+++ src/Timenote.Persistence/Repositories/Abstractions/IProjectRepository.cs
@@ -4,5 +4,11 @@ namespace Timenote.Persistence.Repositories.Abstractions;
 
 public interface IProjectRepository
 {
+    Task<Project> GetByIdAsync(Guid projectId);
+    
+    Task<bool> ExistsAsync(Guid projectId);
+    
     Task<Project> AddAsync(Project project);
+    
+    Task<Project> UpdateAsync(Project project);
 }
--- src/Timenote.Persistence/Repositories/Implementations/ProjectRepository.cs
+++ src/Timenote.Persistence/Repositories/Implementations/ProjectRepository.cs
@@ -7,9 +7,28 @@ namespace Timenote.Persistence.Repositories.Implementations;
 
 internal sealed class ProjectRepository(DatabaseContext context) : IProjectRepository
 {
+    public async Task<Project> GetByIdAsync(Guid projectId)
+    {
+        return await context.Projects.FirstAsync(p => p.Id == projectId);
+    }
+
+    public async Task<bool> ExistsAsync(Guid projectId)
+    {
+        return await context.Projects.AnyAsync(p => p.Id == projectId);
+    }
+
     public async Task<Project> AddAsync(Project project)
     {
         context.Projects.Add(project);
+        
+        await context.SaveChangesAsync();
+        
+        return project;
+    }
+
+    public async Task<Project> UpdateAsync(Project project)
+    {
+        context.Projects.Update(project);
         
         await context.SaveChangesAsync();
         
