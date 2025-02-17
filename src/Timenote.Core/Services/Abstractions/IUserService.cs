c5355dcf00b9f743f35af0cebc235bcdd0634de7
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-02-17 20:28:35 +0100

    Create IUserService with implementation

---
+++ src/Timenote.Core/Services/Abstractions/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using Timenote.Domain.Entities;
+
+namespace Timenote.Core.Services.Abstractions;
+
+public interface IUserService
+{
+    Task<User> CreateUserAsync(User user);
+}
---
+++ src/Timenote.Core/Services/Implementations/UserService.cs
@@ -0,0 +1,13 @@
+﻿using Timenote.Core.Services.Abstractions;
+using Timenote.Domain.Entities;
+using Timenote.Persistence.Repositories.Abstractions;
+
+namespace Timenote.Core.Services.Implementations;
+
+public class UserService(IUserRepository userRepository) : IUserService
+{
+    public async Task<User> CreateUserAsync(User user)
+    {
+        return await userRepository.AddAsync(user);
+    }
+}
