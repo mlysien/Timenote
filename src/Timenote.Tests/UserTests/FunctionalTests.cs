d6e9114bb5a9a810b4d9faf6e04086c0b3a1678e
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-02-17 20:35:31 +0100

    Create unit test for checking user create

--- src/Timenote.Tests/UserTests/FunctionalTests.cs
+++ src/Timenote.Tests/UserTests/FunctionalTests.cs
@@ -1,7 +1,51 @@
-﻿namespace Timenote.Tests.UserTests;
+﻿using Moq;
+using Timenote.Core.Services.Abstractions;
+using Timenote.Core.Services.Implementations;
+using Timenote.Domain.Entities;
+using Timenote.Persistence.Repositories.Abstractions;
 
-[TestFixture(TestName = "Functional tests of User entity scope")]
+namespace Timenote.Tests.UserTests;
+
+[TestFixture(TestName = "Functional tests of User scope")]
 public class FunctionalTests
 {
+    private Mock<IUserRepository> _userRepositoryMock;
+    private IUserService _userService;
+
+    [SetUp]
+    public void Setup()
+    {
+        _userRepositoryMock = new Mock<IUserRepository>();
+        _userService = new UserService(_userRepositoryMock.Object);
+    }
     
+    [Test, Description("Creating new User should invoke repository method for create User entity")]
+    public async Task CreateUser_ShouldInvokeRepositoryMethodForCreateUserEntity()
+    {
+        // arrange
+        var user = new User
+        {
+            Name = "John Doe",
+            Email = "john.doe@timenote.com",
+        };
+
+        _userRepositoryMock.Setup(repository => repository
+            .AddAsync(user)).ReturnsAsync(new User
+        {
+            Id = Guid.NewGuid(),
+            Name = "John Doe",
+            Email = "john.doe@timenote.com",
+        });
+
+        // act
+        var createdUser = await _userService.CreateUserAsync(user);
+        
+        // assert
+        Assert.That(createdUser, Is.Not.Null);
+        Assert.That(createdUser.Id, Is.Not.Empty);
+        Assert.That(createdUser.Name, Is.EqualTo(user.Name));
+        Assert.That(createdUser.Email, Is.EqualTo(user.Email));
+        
+        _userRepositoryMock.Verify(repository => repository.AddAsync(user), Times.Once);
+    }
 }
