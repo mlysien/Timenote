2064de68fc2abd8611672a0f47bf1b83bde83eee
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-02-17 20:28:23 +0100

    Create IUserRepository with implementation

---
+++ src/Timenote.Persistence/Repositories/Abstractions/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using Timenote.Domain.Entities;
+
+namespace Timenote.Persistence.Repositories.Abstractions;
+
+public interface IUserRepository
+{
+    Task<User> AddAsync(User user);
+}
---
+++ src/Timenote.Persistence/Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Timenote.Domain.Entities;
+using Timenote.Persistence.Repositories.Abstractions;
+
+namespace Timenote.Persistence.Repositories.Implementations;
+
+public class UserRepository : IUserRepository
+{
+    public Task<User> AddAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
