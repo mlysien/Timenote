797bbb2c07d33aab6896df0dd4a7963ab572087d
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-02-21 20:01:51 +0100

    Add abstractions for commands

---
+++ src/Timenote.Shared/Messaging/ICommandHandler.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Timenote.Shared.Kernel;
+
+namespace Timenote.Shared.Messaging;
+
+public interface ICommandHandler<in TCommand, TResponse>
+    : IRequestHandler<TCommand, Result<TResponse>>
+    where TCommand : ICommand<TResponse>;
