0cdd4a451156bd655624725b1151d75de24e6540
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-02-22 17:44:44 +0100

    Remove unused interface

--- src/Timenote.Shared/Messaging/ICommand.cs
+++ src/Timenote.Shared/Messaging/ICommand.cs
@@ -3,8 +3,6 @@ using Timenote.Shared.Common;
 
 namespace Timenote.Shared.Messaging;
 
-public interface ICommand : IRequest<Result>, IBaseCommand;
-
 public interface ICommand<TResponse> : IRequest<Result<TResponse>>, IBaseCommand;
 
 public interface IBaseCommand;
