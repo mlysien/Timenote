3a125a97f6206652135d4f3f753d469edc6ef25c
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-02-27 22:28:28 +0100

    UpdateProjectNameCommandHandlerTest.cs

--- src/Timenote.Tests/Projects/Commands/UpdateProjectNameCommandHandlerTest.cs
+++ src/Timenote.Tests/Projects/Commands/UpdateProjectNameCommandHandlerTest.cs
@@ -37,5 +37,34 @@ public class UpdateProjectNameCommandHandlerTest
         Assert.That(result.Value, Is.TypeOf<Guid>());
         Assert.That(result.Value, Is.EqualTo(project.Id));
     }
-
+    
+    [Test]
+    public async Task Handle_ShouldFailure_WhenProjectNotExists()
+    {
+        // arrange
+        var project = new Project
+        {
+            Id = Guid.NewGuid(),
+            Code = "PROJECT.CODE",
+            Name = "Test Project",
+            HoursBudget = 2400
+        };
+        var repositoryMock = new Mock<IProjectRepository>();
+        repositoryMock.Setup(r => r.GetByIdAsync(project.Id)).ReturnsAsync(false);
+        var command = new UpdateProjectNameCommand(project.Id, "Updated Project");
+        var handler = new UpdateProjectNameCommandHandler(repositoryMock.Object);
+    
+        // act
+        var result = await handler.Handle(command, CancellationToken.None);
+        
+        // assert
+        repositoryMock.Verify(r => r.GetByIdAsync(project.Id), Times.Once);
+        repositoryMock.Verify(r => r.UpdateAsync(project), Times.Once);
+        repositoryMock.Verify(r => r.UpdateAsync(It.Is<Project>(p => p.Name == "Updated Project")), Times.Once);
+        
+        Assert.That(result.IsSuccess, Is.True);
+        Assert.That(result.Value, Is.Not.Empty);
+        Assert.That(result.Value, Is.TypeOf<Guid>());
+        Assert.That(result.Value, Is.EqualTo(project.Id));
+    }
 }
