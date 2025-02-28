aca27a60f88b5ddb7bbdc97edd7b2dec910a07a5
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-02-28 08:39:36 +0100

    Add ValueObject for Entities identifiers

---
+++ src/Timenote.Common/ValueObjects/Unique.cs
@@ -0,0 +1,16 @@
+﻿namespace Timenote.Common.ValueObjects;
+
+/// <summary>
+/// Unique identifier for entities
+/// </summary>
+public record Unique(Guid Value)
+{
+    public static implicit operator Guid(Unique unique) => unique.Value;
+
+    public static explicit operator Guid?(Unique unique) => unique.Value;
+    
+    public override string ToString()
+    {
+        return Value.ToString();
+    }
+}
