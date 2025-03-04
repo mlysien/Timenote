05445e3e8148dab45b06106a34b0d1e263decb6f
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-03-04 20:01:28 +0100

    Create AssignUserCommand

---
+++ src/Timenote.Application/Projects/Commands/AssignUser/AssignUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using Timenote.Domain.ValueObjects;
+using Timenote.Shared.Messaging;
+
+namespace Timenote.Application.Projects.Commands.AssignUser;
+
+public record AssignUserCommand(Unique ProjectId, Unique UserId) : ICommand<Unique>;
---
+++ src/Timenote.Application/Projects/Commands/AssignUser/AssignUserCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using Timenote.Domain.ValueObjects;
+using Timenote.Persistence.Repositories.Abstractions;
+using Timenote.Shared.Common;
+using Timenote.Shared.Messaging;
+
+namespace Timenote.Application.Projects.Commands.AssignUser;
+
+internal sealed class AssignUserCommandHandler(IProjectRepository projectRepository)
+    : ICommandHandler<AssignUserCommand, Unique>
+{
+    public Task<Result<Unique>> Handle(AssignUserCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
