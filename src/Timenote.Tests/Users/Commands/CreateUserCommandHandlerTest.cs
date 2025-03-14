670a5512615d594c900059bc95ea2228923700e5
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-03-14 16:46:05 +0100

    Add test for error case when user email already exist

--- src/Timenote.Tests/Users/Commands/CreateUserCommandHandlerTest.cs
+++ src/Timenote.Tests/Users/Commands/CreateUserCommandHandlerTest.cs
@@ -21,7 +21,7 @@ public class CreateUserCommandHandlerTest
         
         var repositoryMock = new Mock<IUserRepository>();
         
-        repositoryMock.Setup(r => r.ExistsAsync(It.IsAny<Unique>())).ReturnsAsync(true);
+        repositoryMock.Setup(r => r.EmailExistsAsync(email)).ReturnsAsync(false);
 
         var command = new CreateUserCommand(userName, email, password);
         var handler = new CreateUserCommandHandler(repositoryMock.Object);
@@ -40,5 +40,32 @@ public class CreateUserCommandHandlerTest
         repositoryMock.Verify(repo => repo.AddAsync(It.Is<User>(
             p => p.Name == userName && p.Email == email && p.Password == password)
         ), Times.Once);
+    }
+    
+    [Test]
+    public async Task Handle_ShouldFailure_WhenUserEmailAlreadyExists()
+    {
+        // arrange
+        const string userName = "John Snow";
+        const string email = "john@snow.com";
+        const string password = "snow123!";
+        
+        var repositoryMock = new Mock<IUserRepository>();
+        
+        repositoryMock.Setup(r => r.EmailExistsAsync(email)).ReturnsAsync(true);
+
+        var command = new CreateUserCommand(userName, email, password);
+        var handler = new CreateUserCommandHandler(repositoryMock.Object);
+    
+        // act
+        var result = await handler.Handle(command, CancellationToken.None);
+        
+        // assert
+        result.IsFailure.ShouldBeTrue();
+        result.Error.Type.ShouldBe(ErrorType.Conflict);
+        result.Error.Message.ShouldNotBeNullOrEmpty();
+        
+        repositoryMock.Verify(r => r.EmailExistsAsync(email), Times.Once);
+        repositoryMock.Verify(r => r.AddAsync(It.IsAny<User>()), Times.Never);
     }
 }
