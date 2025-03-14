ec20aa694428ed9f2c6174f78417b0bda34b5e87
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-03-14 16:52:59 +0100

    Extend Error class to return Failure method

--- src/Timenote.Application/Common/Error.cs
+++ src/Timenote.Application/Common/Error.cs
@@ -10,4 +10,6 @@ public class Error(ErrorType type, string message)
     public static readonly Error NullValue = new(ErrorType.Failure, string.Empty);
     
     public static Error Conflict(string description) => new(ErrorType.Conflict, description);
+    
+    public static Error Failure(string description) => new(ErrorType.Failure, description);
 }
