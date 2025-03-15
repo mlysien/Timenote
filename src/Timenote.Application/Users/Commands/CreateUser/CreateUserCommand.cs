c62eb60671989da882194a2639b5465a1b6bb68f
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-03-15 19:31:40 +0100

    Update CreateUserCommand summary

--- src/Timenote.Application/Users/Commands/CreateUser/CreateUserCommand.cs
+++ src/Timenote.Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -1,13 +1,15 @@
 ﻿using Timenote.Application.Messaging;
+using Timenote.Domain.Entities;
 using Timenote.Domain.ValueObjects;
 
 namespace Timenote.Application.Users.Commands.CreateUser;
 
 /// <summary>
-/// Creates new User entity.
+/// Creates new <see cref="User"/> entity.
 /// </summary>
 /// <param name="Name">User name</param>
 /// <param name="Email">User email (unique)</param>
 /// <param name="Password">User password</param>
 /// <returns>Identifier of created User</returns>
+
 public record CreateUserCommand(string Name, string Email, string Password) : ICommand<Unique>;
