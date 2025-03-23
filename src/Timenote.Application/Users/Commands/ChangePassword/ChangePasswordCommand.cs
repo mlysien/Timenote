1cc6460073482491cb927db75fdd8745a423249b
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-03-23 09:54:38 +0100

    Create ChangePasswordCommand with handler

---
+++ src/Timenote.Application/Users/Commands/ChangePassword/ChangePasswordCommand.cs
@@ -0,0 +1,6 @@
+﻿using Timenote.Application.Messaging;
+using Timenote.Domain.ValueObjects;
+
+namespace Timenote.Application.Users.Commands.ChangePassword;
+
+public record ChangePasswordCommand(Unique UserId, string OldPassword, string NewPassword) : ICommand;
---
+++ src/Timenote.Application/Users/Commands/ChangePassword/ChangePasswordCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using Timenote.Application.Common;
+using Timenote.Application.Messaging;
+
+namespace Timenote.Application.Users.Commands.ChangePassword;
+
+internal sealed class ChangePasswordCommandHandler : ICommandHandler<ChangePasswordCommand>
+{
+    public Task<Result> Handle(ChangePasswordCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
