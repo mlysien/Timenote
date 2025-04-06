fa59cf3786a0113523fa8182f6b7b73e1fb22420
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2025-04-06 11:28:04 +0200

    Add WorkTimeEntry entity

---
+++ src/Timenote.Domain/Entities/WorkTimeEntry.cs
@@ -0,0 +1,16 @@
+﻿using Timenote.Domain.ValueObjects;
+
+namespace Timenote.Domain.Entities;
+
+public sealed class WorkTimeEntry : EntityBase
+{
+    public Unique UserId { get; set; }
+
+    public Unique ProjectId { get; set; }
+
+    public DateTime StartTime { get; set; }
+
+    public DateTime EndTime { get; set; }
+
+    public string? Description { get; set; }
+}
